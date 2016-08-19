3941105d23669c81c16ae187b83b8b345616fe12
Pdragich <paul-dragich@uiowa.edu>
2016-08-18 20:19:19 -0500

    Services and Products page working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+
+$(document).ready(function () {
+	"use strict";
+	$('ul.tabs li').click(function () {
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#" + tab_id).addClass('current');
+	});
+
+});
+
+
+
+/*
 $(document).ready({
 	// Code goes here
-});
+});*/
